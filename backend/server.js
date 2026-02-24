c8497923b5826c064742389d5b55bd5a8a74a2bd
afra-fathi08 <afrafathi08@gmail.com>
2026-02-24 16:28:35 +0530

    home page added

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("SkillScan AI Backend Running 🚀");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
